4aa93df1a11fc509c971c75cec59cf3c1e727a4c
ThanhhDanh <118066468+ThanhhDanh@users.noreply.github.com>
2025-08-22 14:20:57 +0700

    fixed connect monggodb atlas

--- bamboo_be/src/config/db/index.js
+++ bamboo_be/src/config/db/index.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/data_bamboo_dev');
-        console.log("Connected successfully");
+        // await mongoose.connect('mongodb://127.0.0.1:27017/data_bamboo_dev');
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected successfully');
     } catch (error) {
-        console.log("Lỗi: " + error.message);
+        console.log('Lỗi: ' + error.message);
     }
 }
 
