ae197aca3ba80235bc16298334571a493a799b83
ThanhhDanh <118066468+ThanhhDanh@users.noreply.github.com>
2025-08-23 23:35:20 +0700

    test log

--- bamboo_be/src/util/redis.js
+++ bamboo_be/src/util/redis.js
@@ -1,6 +1,9 @@
 const Redis = require('ioredis');
 
-console.log('>>> REDIS_URL:', process.env.REDIS_URL);
+console.log('>>> REDIS_URL ENV RAW:', process.env.REDIS_URL);
+if (!process.env.REDIS_URL) {
+    console.error('❌ REDIS_URL is missing, check Render environment variables!');
+}
 
 const redis = new Redis(process.env.REDIS_URL, {
     tls: {
