a81b2aea81d79329e7a66460d056d26fc7b9b01e
ThanhhDanh <118066468+ThanhhDanh@users.noreply.github.com>
2025-08-24 18:00:01 +0700

    fixed socket

--- bamboo_be/src/util/webhook.js
+++ bamboo_be/src/util/webhook.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 
+const WEBHOOK_URL = process.env.BE_WEBHOOK_URL || 'http://127.0.0.1:3000';
+
 async function sendWebhook(receiverId, message) {
     // Sử dụng webhook của nhân viên dashboard nhận
-    await axios.post('http://127.0.0.1:3000/webhook/chat', {
+    await axios.post(`${WEBHOOK_URL}/webhook/chat`, {
         receiver: receiverId,
         content: message.content,
         time: message.sentAt,
