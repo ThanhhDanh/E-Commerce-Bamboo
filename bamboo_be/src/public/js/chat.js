bf58a216326753fdf99a5ffaaf06e19937bf3be2
ThanhhDanh <118066468+ThanhhDanh@users.noreply.github.com>
2025-08-25 10:24:59 +0700

    fixed socket.io

--- bamboo_be/src/public/js/chat.js
+++ bamboo_be/src/public/js/chat.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const socket = io('http://127.0.0.1:3000');
+    const SOCKET_API = process.env.BE_WEBHOOK_URL || 'http://127.0.0.1:3000';
+    const socket = io(`${SOCKET_API}`);
     const currentUserId = typeof CURRENT_USER_ID !== 'undefined' ? CURRENT_USER_ID : null;
 
     if (currentUserId) {
